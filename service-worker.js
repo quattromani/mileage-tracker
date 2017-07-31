ae2f03a0910328a1f5e54d1edbfc5d1e45baa9dc
quattromani <max@maxquattromani.com>
2017-07-30 20:19:28 -0600

    updated sw version - ugh, missed comma

--- service-worker.js
+++ service-worker.js
@@ -14,15 +14,15 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'mileage-tracker-v2';
+const PRECACHE = 'mileage-tracker-v3';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  '/images/logo.svg'
   './',
   '/index.html',
   '/js/production.min.js',
+  '/images/logo.svg'
 ];
 
 // The install handler takes care of precaching the resources we always need.
