84f47773179caba9b53c869e068249cabf5d662f
quattromani <max@maxquattromani.com>
2017-07-30 20:14:29 -0600

    updated sw

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,7 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '/images/logo.svg'
-  '/',
+  './',
   '/index.html',
   '/js/production.min.js',
 ];
