7e7e5661a4c432582f48ebdca5eac4526d896f3d
quattromani <max@maxquattromani.com>
2017-07-30 20:57:14 -0600

    updated sw

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,35 @@
+// Version 0.57
+let version = '0.57';
+
+self.addEventListener('install', e => {
+  let timeStamp = Date.now();
+  e.waitUntil(
+    caches.open('mileage-tracker').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html?timestamp=${timeStamp}',
+        '/js/production.min.js?timestamp=${timeStamp}',
+        '/images/logo.svg'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+
 const PRECACHE = 'mileage-tracker-v4';
 const RUNTIME = 'runtime';
 
