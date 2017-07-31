2747be3115dbc102c5b509d85baa06c02dcd977c
quattromani <max@maxquattromani.com>
2017-07-30 19:30:41 -0600

    service workers are hard

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,69 @@
-var dataCacheName = 'mileageData-v3';
-var cacheName = 'mileagePWA-final-3';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/js/production.min.js',
+const VERSION = "0.0.3";
+
+const cacheName = 'mileage-tracker';
+const APP_CACHE_NAME = 'mileage-tracker-app';
+const STATIC_CACHE_NAME = 'mileage-tracker-static';
+
+console.log(`installing service-worker.js`);
+
+const CACHE_STATIC = [
   '/images/logo.svg'
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+const CACHE_APP = [
+  '/',
+  '/index.html',
+  '/js/production.min.js',
+];
+
+self.addEventListener('install',function(e){
+    e.waitUntil(
+        Promise.all([
+            caches.open(STATIC_CACHE_NAME),
+            caches.open(APP_CACHE_NAME),
+            self.skipWaiting()
+          ]).then(function(storage){
+            var static_cache = storage[0];
+            var app_cache = storage[1];
+            return Promise.all([
+              static_cache.addAll(CACHE_STATIC),
+              app_cache.addAll(CACHE_APP)]);
+        })
+    );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+    e.waitUntil(
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        if (cacheName !== APP_CACHE_NAME && cacheName !== STATIC_CACHE_NAME) {
+                            console.log('deleting',cacheName);
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
+    );
+});
+
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request)
+    .then(function (match) {
+      return match || fetch(event.request);
+    }).catch(function() {
+      return fetch(event.request);
+    })
+    .then(function(r) {
+      response = r;
+      caches.open(cacheName).then(function(cache) {
+        cache.put(event.request, response);
+      });
+      return response.clone();
     })
   );
-
-  return self.clients.claim();
 });
